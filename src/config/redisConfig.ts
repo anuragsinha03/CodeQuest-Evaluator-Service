c49b383ec68d234c055369c80c36b4cbda6f3784
Anurag Sinha <anuraagrk4@gmail.com>
2024-06-28 23:38:13 +0530

    Feat: BullMQ based redis queue infrastructure is up and running

--- src/config/redisConfig.ts
+++ src/config/redisConfig.ts
@@ -4,6 +4,7 @@ import serverConfig from "./serverConfig";
 const redisConfig = {
 	port: serverConfig.REDIS_PORT,
 	host: serverConfig.REDIS_HOST,
+	maxRetriesPerRequest: null,
 };
 
 const redisConnection = new Redis(redisConfig);
